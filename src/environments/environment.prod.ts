9d171272a92eb2869f5afce54b0faecbefe4cd03
ZivaiiKane <zivaiikane@gmail.com>
2022-04-19 12:39:24 +0200

    feat: :tada: Initial commit
    Initial project setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'brighttravel-e4192',
+    appId: '1:475787947754:web:bc561c4ff85201da383193',
+    storageBucket: 'brighttravel-e4192.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDXOx99oZnxVCoQsRazOF1adCuwIaZAyBw',
+    authDomain: 'brighttravel-e4192.firebaseapp.com',
+    messagingSenderId: '475787947754',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'brighttravel-e4192',
+    appId: '1:475787947754:web:bc561c4ff85201da383193',
+    storageBucket: 'brighttravel-e4192.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDXOx99oZnxVCoQsRazOF1adCuwIaZAyBw',
+    authDomain: 'brighttravel-e4192.firebaseapp.com',
+    messagingSenderId: '475787947754',
+  },
   production: false
 };
 

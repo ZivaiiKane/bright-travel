e5e65335e69ec6284b90d956d4f6043fba2ff267
ZivaiiKane <zivaiikane@gmail.com>
2022-04-24 21:09:10 +0200

    feat: :sparkles: Impliment auth guard
    Implimented auth gard for dashboard page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './features/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './features/auth/sign-up/sign-up.component';
+import { AuthGuard } from './guards/auth.guard';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 const routes: Routes = [
@@ -9,8 +10,9 @@ const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
   {
-    path: 'dashboard',
+    path: '',
     loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule),
+    canActivate: [AuthGuard],
   },
   { path: '**', component: NotFoundComponent },
 ];
--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { TripComponent } from '../features/dashboard/pages/trip/trip.component';
 import { Routes, RouterModule } from '@angular/router';
+import { CalendarComponent } from '../features/dashboard/pages/calendar/calendar.component';
 
-const routes: Routes = [{ path: '', component: TripComponent }];
+const routes: Routes = [
+  { path: 'dashboard', component: TripComponent },
+  { path: 'calendar', component: CalendarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
